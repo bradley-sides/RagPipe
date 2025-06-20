ad0d2520bdbfd23f1efaa527e7347d6b2b9e39c6
Bradley Sides <bsides@zonetrading.com>
2025-06-20 17:21:37 -0400

    weekend

--- src/config.py
+++ src/config.py
@@ -32,5 +32,37 @@ DOCS = [
       "fiscal_year": 2026,
       "quarter":     "Q1",
       "call_date": "2025-05-28",
+    },
+    {
+      "file_path":   "data/2024_Q2_NVTS.pdf",
+      "doc_id":      "nvts_q2_fy2024",
+      "company":     "NVTS",
+      "fiscal_year": 2024,
+      "quarter":     "Q2",
+      "call_date": "2024-08-24",
+    },
+    {
+      "file_path":   "data/2024_Q3_NVTS.pdf",
+      "doc_id":      "nvts_q3_fy2024",
+      "company":     "NVTS",
+      "fiscal_year": 2024,
+      "quarter":     "Q3",
+      "call_date": "2024-11-24",
+    },
+    {
+      "file_path":   "data/2024_Q4_NVTS.pdf",
+      "doc_id":      "nvts_q4_fy2024",
+      "company":     "NVTS",
+      "fiscal_year": 2024,
+      "quarter":     "Q4",
+      "call_date": "2025-02-25",
+    },
+    {
+      "file_path":   "data/2025_Q1_NVTS.pdf",
+      "doc_id":      "nvts_q1_fy2025",
+      "company":     "NVTS",
+      "fiscal_year": 2025,
+      "quarter":     "Q1",
+      "call_date": "2025-05-05",
     }
 ]
